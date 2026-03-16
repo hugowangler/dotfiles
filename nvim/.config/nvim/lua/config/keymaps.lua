8b908e5bdc12247243fef7233c96f015b498171b
Hugo Wangler <hugowangler@gmail.com>
2026-03-16 16:30:30 +0100

    fix: adjust navigation and resizing

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -9,3 +9,8 @@ map("n", "q:", "<nop>", { desc = "Disable command-line window" })
 -- Reload config
 map("n", "<leader>R", "<cmd>source $MYVIMRC<CR>", { desc = "Reload config" })
 
+-- Resize splits
+map("n", "<C-w>H", "<C-w>5<", { desc = "Resize left" })
+map("n", "<C-w>L", "<C-w>5>", { desc = "Resize right" })
+map("n", "<C-w>J", "<C-w>5+", { desc = "Resize down" })
+map("n", "<C-w>K", "<C-w>5-", { desc = "Resize up" })
